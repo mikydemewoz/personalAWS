6d0bc12cf3c1f6eb97b6f135a0515f1395bcaef3
Michael Demewoz <gdmichaeldemewoz@gmail.com>
2023-03-21 03:04:37 -0500

    change index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Admin from './Components/Admin';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter>
-  <Routes>
-    <Route path='/' element={ <App />} />
-    <Route path='/admin' element={<Admin/>}/>
-  </Routes>
-</BrowserRouter>
-    
+  <div>
+    <App />
+  </div>
    
 
 );
